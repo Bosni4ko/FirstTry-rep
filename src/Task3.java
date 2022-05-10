44f47adb0a66c66284b9bcce9b7f1adb3b3e2b45
Bosni4ko <stefanbosnyak9@gmail.com>
2022-05-10 14:56:06 +0300

    Tasks3 & 4- with input handling

--- src/Task3.java
+++ src/Task3.java
@@ -4,8 +4,20 @@ public class Task3 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
+        String input;
+        int n;
         System.out.println("Please input a number: ");
-        int n = sc.nextInt();
+        while (true) {
+            System.out.println("Enter value a");
+            input = sc.next();
+            try {
+                n = Integer.parseInt(input);
+                break;
+            }
+            catch (NumberFormatException e){
+                System.out.println("Incorrect input");
+            }
+        }
         System.out.println(n % 2 == 0);
     }
 }
--- src/Task4.java
+++ src/Task4.java
@@ -5,7 +5,20 @@ public class Task4 {
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please input a number: ");
-        int n = sc.nextInt();
+        String input;
+        int n;
+        System.out.println("Please input a number: ");
+        while (true) {
+            System.out.println("Enter value a");
+            input = sc.next();
+            try {
+                n = Integer.parseInt(input);
+                break;
+            }
+            catch (NumberFormatException e){
+                System.out.println("Incorrect input");
+            }
+        }
         System.out.println(n % 2 != 0);
     }
 }
