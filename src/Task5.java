2aa8195cc68deb983ecb778479d3e1d0270ba5c3
Bosni4ko <stefanbosnyak9@gmail.com>
2022-05-10 14:57:38 +0300

    Tasks5- with input handling

--- src/Task5.java
+++ src/Task5.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class Task5 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter boolean: ");
-        boolean b = sc.nextBoolean();
+        boolean b;
+        while(true){
+            System.out.println("Please enter boolean: ");
+            if(sc.hasNextBoolean()) {
+                b = sc.nextBoolean();
+                break;
+            }
+            else {
+                System.out.println("Incorrect input");
+                sc.nextLine();
+            }
+        }
 
         System.out.println(!b);
 
