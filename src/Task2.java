afb54ec9f954d565eb9f29ab635edf4646d418d7
Bosni4ko <stefanbosnyak9@gmail.com>
2022-05-10 14:51:32 +0300

    Tasks2- with input handling

--- src/Task2.java
+++ src/Task2.java
@@ -5,12 +5,34 @@ public class Task2
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
+        boolean b1,b2;
 
         System.out.println("Please enter 2 booleans: ");
-        System.out.println("Enter value b1");
-        boolean b1 = sc.nextBoolean();
-        System.out.println("Enter value b2");
-        boolean b2 = sc.nextBoolean();
+        while(true){
+            System.out.println("Enter value b1");
+            if(sc.hasNextBoolean()) {
+                b1 = sc.nextBoolean();
+                break;
+            }
+            else {
+                System.out.println("Incorrect input");
+                sc.nextLine();
+            }
+        }
+
+        while(true){
+            System.out.println("Enter value b2");
+            if(sc.hasNextBoolean()) {
+                b2 = sc.nextBoolean();
+                break;
+            }
+            else {
+                System.out.println("Incorrect input");
+                sc.nextLine();
+            }
+        }
+
+
 
         System.out.println("Is b1 and b2 equal? - " + (b1 == b2));
     }
